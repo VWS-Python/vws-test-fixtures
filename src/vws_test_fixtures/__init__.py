844666644a3d88fc4663b2da0a2a8d303a1e4da2
Adam Dangoor <adamdangoor@gmail.com>
2019-12-13 13:48:05 +0000

    Remove duplication

--- src/vws_test_fixtures/__init__.py
+++ src/vws_test_fixtures/__init__.py
@@ -1,12 +1,3 @@
-
-# import pdb; pdb.set_trace()
-#
-# def pytest_runtest_setup(item):
-#     import pdb; pdb.set_trace()
-#     pass
-#
-# def pytest_addoption(parser):
-#
-#     import pdb; pdb.set_trace()
-#     pass
-#
+"""
+XXX
+"""
