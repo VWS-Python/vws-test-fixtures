2ba707a52a82fbdaedb56a3946b8ae94a29b422d
Adam Dangoor <adamdangoor@gmail.com>
2019-12-13 16:49:42 +0000

    More image fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -7,20 +7,28 @@ import io
 pytest_plugins = 'pytester'  # pylint: disable=invalid-name
 
 
-def test_example(
+def test_image_fixtures(
     high_quality_image: io.BytesIO,
     image_file_failed_state: io.BytesIO,
     png_too_large: io.BytesIO,
+    image_file_success_state_low_rating: io.BytesIO,
+    corrupted_image_file: io.BytesIO,
+    image_files_failed_state: io.BytesIO,
+    bad_image_file: io.BytesIO,
+    different_high_quality_image: io.BytesIO,
 ) -> None:
     """
     The image functions can be used as fixtures.
     """
-    high_quality_image_bytes = high_quality_image.getvalue()
-    image_failed_state_bytes = image_file_failed_state.getvalue()
-    png_too_large_bytes = png_too_large.getvalue()
-    fixture_bytes = [
-        high_quality_image_bytes,
-        image_failed_state_bytes,
-        png_too_large_bytes,
+    fixture_bytes_list = [
+        high_quality_image.getvalue(),
+        image_file_failed_state.getvalue(),
+        png_too_large.getvalue(),
+        image_file_success_state_low_rating.getvalue(),
+        corrupted_image_file.getvalue(),
+        image_files_failed_state.getvalue(),
+        bad_image_file.getvalue(),
+        different_high_quality_image.getvalue(),
+
     ]
-    assert len(set(fixture_bytes)) == 3
+    assert len(set(fixture_bytes_list)) == len(fixture_bytes_list)
