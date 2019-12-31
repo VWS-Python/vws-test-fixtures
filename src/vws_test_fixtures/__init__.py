f9a32151c052924e1809b0f96364d579a9732b26
Adam Dangoor <adamdangoor@gmail.com>
2019-12-31 02:24:33 +0000

    Swap versioneer for scm_setuptools

--- src/vws_test_fixtures/__init__.py
+++ src/vws_test_fixtures/__init__.py
@@ -2,7 +2,11 @@
 ``pytest`` fixtures for testing Vuforia Web Services related tools.
 """
 
-from ._version import get_versions
-
-__version__ = get_versions()['version']  # type: ignore
-del get_versions
+try:
+    __version__ = get_version(root='..', relative_to=Path(__file__).parent)
+except LookupError:  # pragma: no cover
+    # When pkg_resources and git tags are not available,
+    # for example in a PyInstaller binary,
+    # we write the file ``_setuptools_scm_version.py`` on ``pip install``.
+    _VERSION_FILE = Path(__file__).parent / '_setuptools_scm_version.txt'
+    __version__ = _VERSION_FILE.read_text()
